6c17858ed975db559cedb6b89e39006ef1800ba2
daffakhairulumam <dapakhairul029@gmail.com>
2025-01-21 09:45:29 +0700

    first commit

--- pages/auth/login.php
+++ pages/auth/login.php
@@ -39,7 +39,7 @@
                         <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
                             <div class="d-flex justify-content-center py-4">
                                 <a href="index.html" class="logo d-flex align-items-center w-auto">
-                                    <img src="../../assets/img/logo.png" alt="">
+                                    <img src="../../assets/img/coffe.png" alt="">
                                     <span class="d-none d-lg-block">Caffe</span>
                                 </a>
                             </div><!-- End Logo -->
